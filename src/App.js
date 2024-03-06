888e5507eec09622190feebba7221b4225e857fc
Peace Oyewole <oyewolepeace94@gmail.com>
2024-03-06 15:48:52 +0100

    Added boostrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-
 import Form from './Form.js';
-import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Table Form</h1>
-      <Form />
+    <div className="container">
+      <div className="App">
+        <h1>Table Form</h1>
+        <Form />
+      </div>
     </div>
   );
 }
